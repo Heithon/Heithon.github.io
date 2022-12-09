d13254720c41089245049b0c5758b084c588718e
zjt <lilzjt@163.com>
2022-12-09 20:14:43 +0800

    Site updated: 2022-12-09 20:14:43

--- js/pjax.js
+++ js/pjax.js
@@ -1,6 +1,7 @@
 /* global NexT, CONFIG, Pjax */
 
 const pjax = new Pjax({
+  // elements: "a",
   selectors: [
     'head title',
     'script[type="application/json"]',
@@ -9,18 +10,61 @@ const pjax = new Pjax({
     '.languages',
     '.pjax',
   ],
-  analytics: false,
+  analytics: true,
   cacheBust: false,
   scrollTo : !CONFIG.bookmark.enable
 });
 
-var newContent = document.querySelector(".charts");
+// var newContent = document.querySelector(".charts");
+// pjax.refresh(newContent);
 
 document.addEventListener('pjax:success', () => {
+
+  // (locals)=> {
+  //   const $ = cheerio.load(locals)
+  //   const post = $('#posts-chart')
+  //   const tag = $('#tags-chart')
+  //   const category = $('#categories-chart')
+  //   const calendar = $('#posts-calendar')
+  //   const radar = $('#categories-radar')
+  //   let htmlEncode = true
+  
+  //   if (post.length > 0 || tag.length > 0 || category.length > 0 || calendar.length > 0 || radar.length > 0) {
+  //     $('head').after('<style type="text/css">#posts-chart,#posts-calendar,#categories-chart,#categories-radar,#tags-chart{width: 100%;height: 300px;margin: 0.5rem auto;padding: 0.5rem;overflow-x: auto;}</style><script type="text/javascript" src="https://cdn.jsdelivr.net/npm/echarts@4.7.0/dist/echarts.min.js"></script>')
+  //     if (post.length > 0 && $('#postsChart').length === 0) {
+  //       if (post.attr('data-encode') === 'true') htmlEncode = true
+  //       post.after(postsChart())
+  //     }
+  //     if (calendar.length > 0 && $('#postsCalendar').length === 0) {
+  //       if (calendar.attr('data-encode') === 'true') htmlEncode = true
+  //       calendar.after(postsCalendar())
+  //     }
+  //     if (tag.length > 0 && $('#tagsChart').length === 0) {
+  //       if (tag.attr('data-encode') === 'true') htmlEncode = true
+  //       tag.after(tagsChart(tag.attr('data-length')))
+  //     }
+  //     if (category.length > 0 && $('#categoriesChart').length === 0) {
+  //       if (category.attr('data-encode') === 'true') htmlEncode = true
+  //       category.after(categoriesChart())
+  //     }
+  //     if (radar.length > 0 && $('#categoriesRadar').length === 0) {
+  //       if (radar.attr('data-encode') === 'true') htmlEncode = true
+  //       radar.after(categoriesRadar())
+  //     }
+  //     if (htmlEncode) {
+  //       return $.root().html().replace(/&amp;#/g, '&#')
+  //     } else {
+  //       return $.root().html()
+  //     }
+  //   } else {
+  //     return locals
+  //   }
+  // }
+  console.log("pjax开始执行")
+
   pjax.executeScripts(document.querySelectorAll('script[data-pjax]'));
   NexT.boot.refresh();
 
-  NexT.boot.refresh(newContent);
 
   // Define Motion Sequence & Bootstrap Motion.
   if (CONFIG.motion.enable) {
@@ -38,4 +82,8 @@ document.addEventListener('pjax:success', () => {
     NexT.utils.activateSidebarPanel(hasTOC ? 0 : 1);
     NexT.utils.updateSidebarPosition();
   }
+
+  var newContent = document.querySelector(".charts.div");
+  console.log("ttt: "+ newContent);
+  pjax.refresh(newContent);
 });
